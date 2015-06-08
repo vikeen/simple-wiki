ef0a6e6f406f5ecde7bff8c6c77a51b19a53d7b7
John Rake <john.rake12@gmail.com>
2015-06-07 22:29:23 -0500

    Removing dependency

--- dist/server/app.js
+++ dist/server/app.js
@@ -11,15 +11,13 @@ var config = require('./config/environment');
 
 // Expose app
 module.exports = function (externalConfig) {
+  config = _.merge(config, externalConfig);
+
   // Setup server
   var app = express();
   var server = require('http').createServer(app);
   require('./config/express')(app);
   require('./routes')(app);
-
-  console.log(config);
-  config = _.merge(config, externalConfig);
-  console.log(config);
 
   server.listen(config.server.port, config.server.ip, function () {
     console.log('Simple Wiki server listening on %d, in %s mode', config.server.port, app.get('env'));
